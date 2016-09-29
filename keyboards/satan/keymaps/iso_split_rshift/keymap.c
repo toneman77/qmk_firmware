0e0147a2d857bd5c17a3d0aa237069d0ca6541d4
Toni <jeder@mail1a.de>
2016-09-29 11:12:34 +0200

    added german "ö" KC_SCLN as a dual role control modifier

--- keyboards/satan/keymaps/iso_split_rshift/keymap.c
+++ keyboards/satan/keymaps/iso_split_rshift/keymap.c
@@ -40,6 +40,7 @@ enum {
 #define CAPSDUAL  CTL_T(KC_ESC)
 #define SPACEDUAL LT(_SPC, KC_SPACE)
 #define ENTERDUAL CTL_T(KC_ENT)
+#define SCLNDUAL  CTL_T(KC_SCLN)
 // arrow cluster duality bottom right corner
 #define ARRLEFT  GUI_T(KC_LEFT)
 #define ARRDOWN  KC_DOWN
@@ -74,11 +75,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `-----------------------------------------------------------'   LEFT DWN RIGHT
      */
     [_DEF] = KEYMAP_ISO_SPLITRSHIFT(
-        KC_GRV,   KC_1,    KC_2,    KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,   \
-        TABDUAL,  KC_Q,    KC_W,    KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,   \
-        CAPSDUAL, KC_A,    KC_S,    KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L,    KC_SCLN, KC_QUOT, KC_NUHS, ENTERDUAL, \
-        KC_LSFT,  KC_NUBS, KC_Z,    KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT,  KC_SLSH, ARRUP,   TG(_SFX),  \
-        KC_LCTL,  KC_LGUI, KC_LALT,             SPACEDUAL,                       KC_RALT, ARRLEFT, ARRDOWN, ARRRIGHT),
+        KC_GRV,   KC_1,    KC_2,    KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9,    KC_0,     KC_MINS, KC_EQL,  KC_BSPC,   \
+        TABDUAL,  KC_Q,    KC_W,    KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O,    KC_P,     KC_LBRC, KC_RBRC, KC_BSLS,   \
+        CAPSDUAL, KC_A,    KC_S,    KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L,    SCLNDUAL, KC_QUOT, KC_NUHS, ENTERDUAL, \
+        KC_LSFT,  KC_NUBS, KC_Z,    KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT,   KC_SLSH, ARRUP,   TG(_SFX),  \
+        KC_LCTL,  KC_LGUI, KC_LALT,             SPACEDUAL,                       KC_RALT,  ARRLEFT, ARRDOWN, ARRRIGHT),
 
     /* Keymap 1: F-and-vim Layer, modified with Space (by holding space)
      * ,-----------------------------------------------------------.
