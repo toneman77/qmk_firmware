6306c272151c340f1ca2258ef5112cec5bb0dc27
Toni <jeder@mail1a.de>
2017-07-04 22:49:19 +0200

    prettier names for the keys

--- keyboards/zeal60/keymaps/iso_split_rshift/keymap.c
+++ keyboards/zeal60/keymaps/iso_split_rshift/keymap.c
@@ -14,10 +14,11 @@
 #define _SFX 3
 
 // dual-role shortcuts
-#define TABDUAL   LT(_TAB, KC_TAB)
-#define CAPSDUAL  CTL_T(KC_ESC)
-#define SPACEDUAL LT(_SPC, KC_SPACE)
-#define ENTERDUAL CTL_T(KC_ENT)
+#define DL_TAB   LT(_TAB, KC_TAB)
+#define DL_CAPS  CTL_T(KC_ESC)
+#define DL_SPACE LT(_SPC, KC_SPACE)
+#define DL_ENTER CTL_T(KC_ENT)
+#define DL_SCLN  CTL_T(KC_SCLN)
 // arrow cluster duality bottom right corner
 #define ARRLEFT  ALT_T(KC_LEFT)
 #define ARRDOWN  GUI_T(KC_DOWN)
@@ -53,10 +54,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
     [_DEF] = {
         {KC_GRV,   KC_1,    KC_2,    KC_3,    KC_4,     KC_5,   KC_6,    KC_7,      KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC   },
-        {TABDUAL,  KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,   KC_Y,    KC_U,      KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, ENTERDUAL },
-        {CAPSDUAL, KC_A,    KC_S,    KC_D,    KC_F,     KC_G,   KC_H,    KC_J,      KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS, _______   },
+        {DL_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,   KC_Y,    KC_U,      KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, DL_ENTER  },
+        {DL_CAPS,  KC_A,    KC_S,    KC_D,    KC_F,     KC_G,   KC_H,    KC_J,      KC_K,    KC_L,    DL_SCLN, KC_QUOT, KC_NUHS, _______   },
         {KC_LSFT,  KC_NUBS, KC_Z,    KC_X,    KC_C,     KC_V,   KC_B,    KC_N,      KC_M,    KC_COMM, KC_DOT,  KC_SLSH, ARRUP,   TG(_SFX)  },
-        {KC_LCTL,  KC_LGUI, KC_LALT, _______, _______, _______, _______, SPACEDUAL, _______, _______, KC_RALT, ARRLEFT, ARRDOWN, ARRRIGHT  }
+        {KC_LCTL,  KC_LGUI, KC_LALT, _______, _______, _______, _______, DL_SPACE,  _______, _______, KC_RALT, ARRLEFT, ARRDOWN, ARRRIGHT  }
     },
 
     /* Keymap 1: F-and-vim Layer, modified with Space (by holding space)
