4574ea701ec639a707e93a8bc8fbdd1c0fbffc7e
Toni <jeder@mail1a.de>
2017-10-19 08:05:29 +0200

    toni build script had to be adjusted to the new make options

--- build-toni.sh
+++ build-toni.sh
@@ -10,13 +10,13 @@ KEYB=$1
 echo "We need sudo later"
 sudo ls 2>&1 /dev/null
 
-make ${KEYB}-${KMAP}-clean
-make ${KEYB}-${KMAP} ${THREADS}
+make ${KEYB}:${KMAP}:clean
+make ${KEYB}:${KMAP} ${THREADS}
 if [[ $? -eq 0 ]]
 then
     echo "please trigger flashing!"
 #   wait_bootloader
-    sudo make ${KEYB}-${KMAP}-dfu ${THREADS}
+    sudo make ${KEYB}:${KMAP}:dfu ${THREADS}
 else
     echo "make failed"
     exit 77
